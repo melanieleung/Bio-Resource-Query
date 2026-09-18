7685af886a7ff0a45813fa9454144bd91ecb4079
Melanie Leung <leungmelanie.s@gmail.com>
2026-09-18 15:23:03 -0700

    Update tool/script
    Last version I uploaded had conflicting merges so ignore it. this is the updated one with the problems resolved as  described in the last commit

--- Bio Database Search.py
+++ Bio Database Search.py
@@ -112,11 +112,10 @@ if FederalClip:
 
     for layer, selection_layer in selection_layers.items():
         output_name = input_layers[layer]
-        output_path = f"{OutputGDB}\\Seg{Subsegment}_{Dist}_FederalClip_{output_name}"
-    arcpy.management.CopyFeatures(selection_layer, output_path)
+        output_path = f"{OutputGDB}\\Seg{Subsegment}_{FileNameDist}_FederalClip_{output_name}"
 
-    # Converts each feature class in the output GDB to KMZ
-    arcpy.env.workspace = OutputGDB
+        arcpy.management.CopyFeatures(selection_layer, output_path)
+        layer_name = f"{output_name}_layer"
 
         arcpy.MakeFeatureLayer_management(output_path, layer_name)
         output_kmz = os.path.join(f"{out_kmz_file}", f"Seg{Subsegment}_{FileNameDist}_FederalClip_{output_name}" + ".kml")
@@ -177,25 +176,20 @@ else:
 
     for layer, selection_layer in selection_layers.items():
         output_name = input_layers[layer]
-        output_path = f"{OutputGDB}\\Seg{Subsegment}_{Dist}_{output_name}"
-    arcpy.management.CopyFeatures(selection_layer, output_path)
-    print(f"Exported {selection_layer} to {output_path}")
+        output_path = f"{OutputGDB}\\Seg{Subsegment}_{FileNameDist}_{output_name}"
 
-    # Converts each feature class in the output GDB to KMZ
-    arcpy.env.workspace = OutputGDB
+        arcpy.management.CopyFeatures(selection_layer, output_path)
+        layer_name = f"{output_name}_layer"
 
-    for fc in arcpy.ListFeatureClasses():
-        gdb_prefix = f"Seg{Subsegment}_{Dist}"
-    else:
-        output_kmz = os.path.join(f"{out_kmz_file}", fc + ".kml")
-    if fc.startswith(f"{gdb_prefix}"):
-        layer_name = f"{fc}_layer"
-        arcpy.MakeFeatureLayer_management(fc, layer_name)
-        arcpy.conversion.LayerToKML(layer_name,
-                                    output_kmz,
-                                    layer_output_scale=0,
-                                    is_composite="NO_COMPOSITE",
-                                    image_size=1024,
-                                    dpi_of_client=96,
-                                    ignore_zvalue="CLAMPED_TO_GROUND")
+        arcpy.MakeFeatureLayer_management(output_path, layer_name)
+        output_kmz = os.path.join(f"{out_kmz_file}", f"Seg{Subsegment}_{FileNameDist}_{output_name}" + ".kml")
+
+        arcpy.conversion.LayerToKML(
+            layer_name,
+            output_kmz,
+            layer_output_scale=0,
+            is_composite="NO_COMPOSITE",
+            dpi_of_client=96,
+            ignore_zvalue="CLAMPED_TO_GROUND"
+            )
         arcpy.Delete_management(layer_name)
